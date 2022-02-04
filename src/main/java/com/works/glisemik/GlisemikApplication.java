9930ca9685db4a542d8ad9951fa2cf8216ea4898
Dogukan Kertlez <dogukankertlez562@hotmail.com>
2022-02-04 19:09:42 +0300

    Update

---
+++ src/main/java/com/works/glisemik/GlisemikApplication.java
@@ -0,0 +1,13 @@
+package com.works.glisemik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlisemikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlisemikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/works/glisemik/GlisemikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.works.glisemik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlisemikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
